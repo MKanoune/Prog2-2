e4c6e4fdf12cdf3ee0d061c64ac4667354cb3431
captain <maximilian.klimm@hs-augsburg.de>
2016-10-06 20:23:45 +0200

    first commit

---
+++ src/Entities/Entity.java
@@ -0,0 +1,10 @@
+package Entities;
+
+/**
+ * Created by Max on 06.10.2016.
+ */
+public abstract class Entity {
+    private int id;
+    private int energy;
+    private XY position;
+}
---
+++ src/Entities/XY.java
@@ -0,0 +1,20 @@
+package Entities;
+
+/**
+ * Created by Max on 06.10.2016.
+ */
+public class XY {
+    private final int x;
+    private final int y;
+
+    public XY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+
+
+}
